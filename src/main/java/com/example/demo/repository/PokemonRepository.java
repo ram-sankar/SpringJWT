d971d50dfe8469d547472d72f4d844ac19e65c85
ram-sankar <mailtoramsankar@gmail.com>
2024-08-05 08:13:38 +0530

    Deleted unwanted files

--- src/main/java/com/example/demo/repository/PokemonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.models.Pokemon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PokemonRepository extends JpaRepository<Pokemon, Long> { //Long is primary key's type
-//    Pokemon createPokemon(Pokemon pokemon);
-}
